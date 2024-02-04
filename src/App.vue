9e9197b04c7e2d94e5032fcd944f6c8e657e6009
kawksung <alstjd5479@naver.com>
2024-02-04 18:48:56 +0900

    feat: AboutView 구현

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheHeader />
+  <TheView />
+</template>
+
+<script setup lang="ts">
+import TheHeader from "@/layouts/TheHeader.vue";
+import TheView from "@/layouts/TheView.vue";
+</script>
+
+<style></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h2>About View</h2>
+    <p>현재 router = "{{ $route.path }}"</p>
+    <button class="btn btn-primary" @click="$router.push('/')">
+      Home으로 이동
+    </button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+const route = useRoute();
+
+console.log(route.path);
+</script>
 
 <style>
 @media (min-width: 1024px) {
