c27b882ed6be20478a8a1a7fa735b9b1f0f8427f
kawksung <alstjd5479@naver.com>
2024-02-04 18:48:29 +0900

    feat: Header 및 기본 View 구현

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-sm bg-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Vue3-Board</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <RouterLink class="nav-link active" to="/">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/about">About</RouterLink>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search" />
+            <button class="btn btn-outline-light" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
