c3dedd5e22b60afeba1b072de6f4701b42b3e8c9
kawksung <alstjd5479@naver.com>
2024-02-04 19:28:50 +0900

    feat: 게시글 더미 데이터 생성

---
+++ src/api/posts.ts
@@ -0,0 +1,20 @@
+// axios
+
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+}
+
+const posts: Post[] = [
+  { id: 1, title: "제목1", content: "내용1", createdAt: "2023-09-08" },
+  { id: 2, title: "제목2", content: "내용2", createdAt: "2023-09-08" },
+  { id: 3, title: "제목3", content: "내용3", createdAt: "2023-09-08" },
+  { id: 4, title: "제목4", content: "내용4", createdAt: "2023-09-08" },
+  { id: 5, title: "제목5", content: "내용5", createdAt: "2023-09-08" },
+];
+
+export function getPosts(): Post[] {
+  return posts;
+}
